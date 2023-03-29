42121454f1905811f4e51268a83054219578a9d6
SAJ33L <sajeel.khan01@gmail.com>
2023-03-29 14:36:49 +0500

    complteted task1 with a minimal front-end

--- Task1/app.py
+++ Task1/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route("/")
 @app.route("/home")
@@ -11,7 +11,18 @@ def home():
 def result():
     output = request.form.to_dict()
     name = output["name"]
-    return render_template("index.html", name = name)
+    city = output["city"]
+    age = output["age"]
+    school = output["school"]
+    session_one = output["session_one"]
+    college = output["college"]
+    session_two = output["session_two"]
+    university = output["university"]
+    session_three = output["session_three"]
+
+    return render_template("bio.html", name = name, age = age, city = city,
+                           school = school, college = college, university = university, session_one = session_one,
+                           session_two = session_two, session_three = session_three)
 
 
 if __name__ == '__main__':
